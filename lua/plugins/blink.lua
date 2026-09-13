896fb4271bd3eeaef318468f4bc3e654ea9a57f2
ahmad <ahmadsaeed3290@gmail.com>
2026-09-13 05:15:07 +0500

    change noice

---
+++ lua/plugins/blink.lua
@@ -0,0 +1,18 @@
+return {
+  "saghen/blink.cmp",
+  opts = {
+    signature = {
+      enabled = false,
+      window = {
+        min_width = 1,
+        max_width = 60,
+        max_height = 10,
+      },
+      completion = {
+        documentation = {
+          auto_show = false,
+        },
+      },
+    },
+  },
+}
---
+++ lua/plugins/noice.lua
@@ -0,0 +1,18 @@
+return {
+  "folke/noice.nvim",
+  opts = {
+    lsp = {
+      signature = {
+        enabled = true,
+      },
+    },
+    views = {
+      hover = {
+        size = {
+          max_width = 60,
+          max_height = 15,
+        },
+      },
+    },
+  },
+}
